7a49cbd71d2b2d8746308ef57209240dbe6079ef
nevres91 <nevres_muratovic@hotmail.com>
2025-06-13 11:45:05 +0200

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import logo from "../public/logo.png";
+
+function App() {
+  return (
+    <>
+      <img className="w-[200px] h-[200px]" src={logo} alt="" />
+      <div>Portfolio</div>
+    </>
+  );
+}
+
+export default App;
